c1b27a587872114cfda71488c874d0f446a6ab55
Nils Haberkamp <n.haberkamp@shopware.com>
2020-07-16 23:00:24 +0200

    add and configure vue-i18n

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "snippets",
+      enableInSFC: true
+    }
+  }
 };
