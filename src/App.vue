fd83444a207421908b85c00e9e02ed83198c4274
Nils Haberkamp <n.haberkamp@shopware.com>
2020-07-17 15:48:05 +0200

    add header and site logo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
-    <div class="sticky-footer"></div>
+    <div class="sticky-footer">
+      <TheHeader />
+    </div>
     <TheFooter />
   </div>
 </template>
 
 <script>
+  import TheHeader from './components/TheHeader';
   import TheFooter from './components/TheFooter';
 
   export default {
     name: 'App',
     components: {
+      TheHeader,
       TheFooter
     }
   };
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="w-screen py-6">
+    <div class="container">
+      <a href="/" class="btn is--inline is--ghost not-italic">
+        <h1 class="text-medium inline-block mr-4">Coloury</h1>
+
+        <span class="inline-block" style="transform: rotateY(180deg)">🌈</span>
+      </a>
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
